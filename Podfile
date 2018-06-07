787c75605d1fd7dab369f4d0121e6a52401944b1
Kriangkrai Phanjan <little@codium.co>
2018-06-07 16:36:16 +0700

    first commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '11.3'
+
+target 'TwitterLBTA' do
+  use_frameworks!
+  pod 'PKHUD'
+  pod 'DeckTransition'
+  pod 'PopupDialog'
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'SnapKit'
+end
---
+++ TwitterLBTA/ViewControllers/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  TwitterLBTA
+//
+//  Created by Kriangkrai Phanjan on 6/6/18.
+//  Copyright © 2018 Kriangkrai Phanjan. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
